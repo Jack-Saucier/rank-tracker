af9cd6f4e21963e8bad69864bc122ae9aeea1348
Jack Saucier <jacksonbsaucier@gmail.com>
2026-07-12 12:25:01 -0400

    Add Google site verification meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,20 +4,23 @@ import NextUpdateTimer from '@/components/NextUpdateTimer';
 export const metadata = {
   title: "trackd",
   description: "Compare your in-game ranks with friends, classmates, and family.",
+  verification: {
+    google: "5BMX4LpWuRkrsIQhwvgWxI2nsDbmG2Ky6k0351P_VeI",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-  	<div className="bg-shape bg-shape-1"></div>
-  	<div className="bg-shape bg-shape-2"></div>
-  	<div className="bg-shape bg-shape-3"></div>
-  	<div className="bg-shape bg-shape-4"></div>
-  	<div className="bg-shape bg-shape-5"></div>
-  	<div className="bg-shape bg-shape-6"></div>
+        <div className="bg-shape bg-shape-1"></div>
+        <div className="bg-shape bg-shape-2"></div>
+        <div className="bg-shape bg-shape-3"></div>
+        <div className="bg-shape bg-shape-4"></div>
+        <div className="bg-shape bg-shape-5"></div>
+        <div className="bg-shape bg-shape-6"></div>
         <NextUpdateTimer />
-  	{children}
+        {children}
       </body>
     </html>
   );
